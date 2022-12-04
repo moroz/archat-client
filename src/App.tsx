39f825f68ab4fafdcee8d2f29ea4c2dc92cf5d15
Karol Moroz <k.j.moroz@gmail.com>
2022-12-04 22:28:46 +0100

    Get video stream to display locally

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import "./App.css";
+import * as Peer from "simple-peer/simplepeer.min.js";
+import { useCallback, useRef } from "react";
+
+function getVideoStream() {
+  return navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+  });
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const sourceVideoRef = useRef<HTMLVideoElement | null>(null);
+  const targetVideoRef = useRef<HTMLVideoElement | null>(null);
+
+  const peer1ref = useRef<Peer.Instance | null>();
+  const peer2ref = useRef<Peer.Instance | null>();
+
+  const onStart = useCallback(async () => {
+    const stream = await getVideoStream();
+    peer1ref.current = new Peer({ initiator: true, stream });
+    peer2ref.current = new Peer();
+
+    peer1ref.current.on("signal", (data: any) => {
+      console.log("peer 1 signal", data);
+      peer2ref.current?.signal(data);
+    });
+
+    peer2ref.current?.on("signal", (data: any) => {
+      console.log("peer 2 signal", data);
+      peer1ref.current?.signal(data);
+    });
+
+    peer2ref.current.on("stream", (stream: any) => {
+      targetVideoRef.current!.srcObject = stream;
+    });
+
+    try {
+      if (sourceVideoRef.current) {
+        sourceVideoRef.current.srcObject = stream;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }, [sourceVideoRef.current, targetVideoRef.current]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid">
+        <video autoPlay ref={sourceVideoRef} />
+        <video autoPlay ref={targetVideoRef} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button type="button" onClick={onStart}>
+        Start chat
+      </button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
